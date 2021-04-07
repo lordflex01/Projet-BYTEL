1febbbd39c07610c472de902831540f38a434e42
ytakadou <youssouftakadoum@gmail.com>
2021-04-07 17:58:32 +0100

    datattable presque done

--- src/Security/AppCustomAuthenticator.php
+++ src/Security/AppCustomAuthenticator.php
@@ -71,7 +71,7 @@ class AppCustomAuthenticator extends AbstractFormLoginAuthenticator implements P
 
         if (!$user) {
             // fail authentication with a custom error
-            throw new CustomUserMessageAuthenticationException('Email could not be found.');
+            throw new CustomUserMessageAuthenticationException('Votre e-mail ou mot de passe est incorrecte.');
         }
 
         return $user;
