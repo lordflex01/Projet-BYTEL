3bb562061f0104d70d8336e8ffb48e7ab3865d47
ytakadou <youssouftakadoum@gmail.com>
2021-04-23 10:53:59 +0000

    footer dans le header, les champs label unic, test sont fait (marche)

--- src/Entity/CodeProjet.php
+++ src/Entity/CodeProjet.php
@@ -5,10 +5,16 @@ namespace App\Entity;
 use App\Repository\CodeProjetRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
  * @ORM\Entity(repositoryClass=CodeProjetRepository::class)
+ * @UniqueEntity(
+ * fields={"libelle"},
+ * errorPath="libelle",
+ * message="Il semble que vous avez déjà crée ce code projet."
+ *)
  */
 class CodeProjet
 {
