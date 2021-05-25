97a9f6516673af598012287d02cb66f96421c5d0
Douae Chtioui <douae.chtioui@gmail.com>
2021-05-25 16:45:32 +0100

    before

--- public/assets/js/utilImputation.js
+++ public/assets/js/utilImputation.js
@@ -28,48 +28,31 @@ $("#btnRech").click(function () {
     dataType: "json",
     async: true,
     success: function (data, status) {
-      var e = $('<th></th><th style="width: 40px">Lun</th><th  style="width: 40px">Mar</th><th  style="width: 40px">Mer</th><th  style="width: 40px">Jeu</th>' +
-        '<th  style="width: 40px">Vend</th>' +
-        '<th  style="width: 40px;color:red">Total</th>' + '<th  style="width: 40px;color:green">Commentaire</th>');
-      $('#entete').html('');
-      $('#entete').append(e);
-      var div1 = $(
-        '<div style="background-color:red"><tr><td><span>Semaine</span></td></tr></div>'
+      var e = $(
+        '<th><button id="addScnt" type="button" class="btn btn-block btn-info btn-sm" style="width: 30px;margin-bottom: -7px;border-radius: 30px;"><i class="fa fa-plus"></i></button></th><th style="width: 40px">Lun</th><th  style="width: 40px">Mar</th><th  style="width: 40px">Mer</th><th  style="width: 40px">Jeu</th>' +
+          '<th  style="width: 40px">Vend</th>' +
+          '<th  style="width: 40px;color:red">Total</th>' +
+          '<th  style="width: 40px;color:green">Commentaires</th>'
       );
-      var es = $(
-        '<tr><td><span>Code Projet</span></td><td style="width: 40px"><input type="number" min="0" max="1" step="0.25" class="form-control-imput"></td>' +
-          '<td  style="width: 40px"><input type="number" min="0" max="1" step="0.25" class="form-control-imput"></td>' +
-          '<td  style="width: 40px"><input type="number" min="0" max="1" step="0.25" class="form-control-imput"></td>' +
-          '<td  style="width: 40px"><input type="number" min="0" max="1" step="0.25" class="form-control-imput"></td>' +
-          '<td  style="width: 40px"><input type="number" min="0" max="1" step="0.25" class="form-control-imput"></td>' +
-          '<td style="width: 40px"><input type="number" min="0" max="1" step="0.25" class="form-control-imput"></td>' +
-          '<td  style="width: 40px"><input type="number" min="0" max="1" step="0.25" class="form-control-imput"></td>' +
-          ' <td  style="width: 40px;color:red"><input type="number" min="0" max="1" step="0.25" class="form-control-imput"></td></tr>'
-      );
-      var div2 = $(
-        '<tr><td><button  style="width: 125%;"class="btn btn-outline-info btn-block btn-sm"><i class="fa fa-plus"></i>&nbsp;Ajouter une nouvelle imputation</button></td></tr>'
-      );
-      $("#tableB").html("");
-      // $("#tableB").append(div1);
-
-      //$('#tableB').append(es);
-      //alert(data);
+     
+      $("#entete").html("");
+      $("#entete").append(e);
       var a = 0;
       var c = 0;
       var t = 0;
       var obj = jQuery.parseJSON(data);
+
       $.each(obj, function (key, value) {
-        m = value.date.date.split('-');
-        // if (id == value.user && month == m[1] && year == m[0]) {
+        m = value.date.date.split("-");
         W = value.week;
-        if (id == value.user && month == W) {
+        if (id == value.user && week == W) {
           if (a != value.tache) {
             $("#tableB").append(
               "<th><span>Code Projet: " + value.codeprojet + "</span></th>"
             );
           }
           $("#tableB").append(
-            '<th style="width: 40px"><input type="number" min="0" max="1" step="0.25" class="form-control-imput" value=' +
+            '<th><input type="number" min="0" max="1" step="0.25" class="form-control-imput" value=' +
               value.valeur +
               "></th>"
           );
@@ -77,34 +60,47 @@ $("#btnRech").click(function () {
           c = c + 1;
           a = value.tache;
           t = t + value.valeur;
-          //'<tr><td id = "user">' + value.codeprojet + ' ' + value.tache + '</td><td id = "date">' + value.date.date + '</td><td id = "valeur">' + value.valeur + '</td></tr>'
         }
       });
-
-      if (c == 0) {
-      }
-      else {
+      if (c > 0) {
         // IMPUT VIDE
         while (c < 5) {
-          $('#tableB').append('<th style="width: 40px"><input type="number" min="0" max="1" step="0.25" class="form-control-imput" value="0"></th>');
+          $("#tableB").append(
+            '<th><input type="number" min="0" max="1" step="0.25" class="form-control-imput" value="0"></th>'
+          );
           c = c + 1;
         }
         //TOTALE
         $("#tableB").append(
-          '<th style="width: 40px"><input type="number" min="0" max="1" step="0.25" class="form-control-imput" value=' +
+          '<th><input type="number" min="0" max="1" step="0.25" class="form-control-imput" value=' +
             t +
             "></th>"
         );
       }
-
-      for (i = 0; i < data.length; i++) {
-        imput = data[i];
-        var e = $('<tr><td id = "user"></td><td id = "dateVS"></td></tr>');
-
-        $("#user", e).html(imput["user"]);
-        //$('#dateVS', e).html(imput['dateVs']);
-        $("#student").append(e);
-      }
+      
+      var scntDiv = $("#tableB");
+      var i = $("#tableB tr").length + 1;
+      $("#addScnt").click(function () {
+        scntDiv.append(
+          '<tr><td><select name="type" class="form-control select2" id="type"></select></td>'+
+          '<td><input type="number" min="0" max="1" step="0.25" class="form-control-imput" value="0"></td>'+
+          '<td><input type="number" min="0" max="1" step="0.25" class="form-control-imput" value="0"></td>'+
+          '<td><input type="number" min="0" max="1" step="0.25" class="form-control-imput" value="0"></td>'+
+          '<td><input type="number" min="0" max="1" step="0.25" class="form-control-imput" value="0"></td>'+
+          '<td><input type="number" min="0" max="1" step="0.25" class="form-control-imput" value="0"></td>'+
+          '<td><input type="number" min="0" max="1" step="0.25" class="form-control-imput" value="0"></td>'+
+          '<td><a href="" id="remScnt">Remove</a></td></tr>'
+        );
+        i++;
+        return false;
+      });
+      $(document).on('click', '#remScnt', function() {
+        if (i > 2) {
+            $(this).closest('tr').remove();
+            i--;
+        }
+        return false;
+    });
     },
     error: function (xhr, textStatus, errorThrown) {
       alert(xhr.responseText);
