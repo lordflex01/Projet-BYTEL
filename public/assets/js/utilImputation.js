cb65ea5bb5db41b59f278d8f12a6b6150bc7ca61
Douae Chtioui <douae.chtioui@gmail.com>
2021-05-28 12:11:34 +0100

    before

--- public/assets/js/utilImputation.js
+++ public/assets/js/utilImputation.js
@@ -20,22 +20,22 @@ $("#btnRech").click(function () {
   let parseDates = (inp) => {
     let year = parseInt(inp.slice(0, 4), 10);
     let week = parseInt(inp.slice(6), 10);
-    
+
     let day = (1 + (week - 1) * 7); // 1st of January + 7 days for each week
-    
+
     let dayOffset = new Date(year, 0, 1).getDay(); // we need to know at what day of the week the year start
-    
+
     dayOffset--; // depending on what day you want the week to start increment or decrement this value. This should make the week start on a monday
-    
+
     let days = [];
     for (let i = 0; i < 7; i++)  // do this 7 times, once for every day
-    days.push(new Date(year, 0, day - dayOffset + i));
-     // add a new Date object to the array with an offset of i days relative to the first day of the week
+      days.push(new Date(year, 0, day - dayOffset + i));
+    // add a new Date object to the array with an offset of i days relative to the first day of the week
     return days;
-    }
-    let week = document.querySelector('#date-input');
-    let dates = parseDates(week.value);
-    alert(dates);
+  }
+  let week = document.querySelector('#date-input');
+  let dates = parseDates(week.value);
+  //alert(dates);
   var date = $("#date-input").val().split("-");
   week = date[1];
   year = date[0];
@@ -50,13 +50,15 @@ $("#btnRech").click(function () {
     success: function (data, status) {
       var e = $(
         '<th><button id="addScnt" type="button" class="btn btn-block btn-info btn-sm" style="width: 30px;margin-bottom: -7px;border-radius: 30px;"><i class="fa fa-plus"></i></button></th><th style="width: 40px">Lun</th><th  style="width: 40px">Mar</th><th  style="width: 40px">Mer</th><th  style="width: 40px">Jeu</th>' +
-          '<th  style="width: 40px">Vend</th>' +
-          '<th  style="width: 40px;color:red">Total</th>' +
-          '<th  style="width: 40px;color:green">Commentaires</th>'
+        '<th  style="width: 40px">Vend</th>' +
+        '<th  style="width: 40px;color:red">Total</th>' +
+        '<th  style="width: 40px;color:green">Commentaires</th>'
       );
-     
+
       $("#entete").html("");
       $("#entete").append(e);
+      
+      $("#tableB").html("");
       var a = 0;
       var c = 0;
       var t = 0;
@@ -72,10 +74,12 @@ $("#btnRech").click(function () {
               "<th><span>Code Projet: " + value.codeprojet + "</span></th>"
             );
           }
-          $("#tableB").append(
+          var L = $(
             '<th><input type="number" min="0" max="1" step="0.25" class="form-control-imput" value=' +
-              value.valeur +
-              "></th>"
+            value.valeur +
+            '></th>'
+          );
+          $("#tableB").append(L
           );
 
           c = c + 1;
@@ -103,28 +107,42 @@ $("#btnRech").click(function () {
       var i = $("#tableB tr").length + 1;
       $("#addScnt").click(function () {
         scntDiv.append(
-          '<tr><td><select class="form-control select2"><option>Douae</option></select></td>'+
-          '<td><input type="number" min="0" max="1" step="0.25" class="form-control-imput" value="0"></td>'+
-          '<td><input type="number" min="0" max="1" step="0.25" class="form-control-imput" value="0"></td>'+
-          '<td><input type="number" min="0" max="1" step="0.25" class="form-control-imput" value="0"></td>'+
-          '<td><input type="number" min="0" max="1" step="0.25" class="form-control-imput" value="0"></td>'+
-          '<td><input type="number" min="0" max="1" step="0.25" class="form-control-imput" value="0"></td>'+
-          '<td><input type="number" min="0" max="1" step="0.25" class="form-control-imput" value="0"></td>'+
-          '<td><input type="text"  style="max-width: 200px" class="form-control-imput"></td>'+
+          '<tr><td><select class="form-control select2"><option>Douae</option></select></td>' +
+          '<td><input type="number" min="0" max="1" step="0.25" class="form-control-imput" value="0"></td>' +
+          '<td><input type="number" min="0" max="1" step="0.25" class="form-control-imput" value="0"></td>' +
+          '<td><input type="number" min="0" max="1" step="0.25" class="form-control-imput" value="0"></td>' +
+          '<td><input type="number" min="0" max="1" step="0.25" class="form-control-imput" value="0"></td>' +
+          '<td><input type="number" min="0" max="1" step="0.25" class="form-control-imput" value="0"></td>' +
+          '<td><input type="number" min="0" max="1" step="0.25" class="form-control-imput" value="0"></td>' +
+          '<td><input type="text"  style="max-width: 200px" class="form-control-imput"></td>' +
           '<td><i class="fa fa-trash" style="font-size: 16px;margin-top: 8px;color: #cc1919;"id="remScnt"></i></td>'
         );
         i++;
         return false;
-        
+        //DEBUT change
+
+        $(document).on('eventChange', (L) => {
+          /* let url = `/api/${e.event.id
+             }/edit`
+           let donnees = {
+             "valeur": e.event.title
+           }
+           let xhr = new XMLHttpRequest
+           xhr.open("PUT", url)
+           xhr.send(JSON.stringify(donnees))*/
+        }
+
+        )
+        //FIN Changes
       });
-      $(document).on('click', '#remScnt', function() {
+      $(document).on('click', '#remScnt', function () {
         if (i > 2) {
-            $(this).closest('tr').remove();
-            i--;
+          $(this).closest('tr').remove();
+          i--;
         }
         return false;
-    });
-  
+      });
+
     },
     error: function (xhr, textStatus, errorThrown) {
       alert(xhr.responseText);
