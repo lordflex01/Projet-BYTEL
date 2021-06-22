b61b2d9635bc04622bf14a054ae60a46c7803f23
Douae Chtioui <douae.chtioui@gmail.com>
2021-06-22 14:47:57 +0100

    export done

--- public/assets/js/utilImputation.js
+++ public/assets/js/utilImputation.js
@@ -8,7 +8,7 @@ var nombresimputation = 0;
 var compteurligneajout = 0;
 $(document).ready(function () {
   $("#btnRech").click(function () {
-    var parseDates = (inp) => {
+      var parseDates = (inp) => {
       let year = parseInt(inp.slice(0, 4), 10);
       let week = parseInt(inp.slice(6), 10);
 
@@ -718,5 +718,50 @@ $(document).ready(function () {
   $(document).on("click", "#suppRow", function () {
     compteurligneajout--;
     $(this).closest("tr").remove();
+  });
+});
+$(document).ready(function () {
+  $("#export").click(function () {
+      var parseDates = (inp) => {
+      let year = parseInt(inp.slice(0, 4), 10);
+      let week = parseInt(inp.slice(6), 10);
+
+      let day = 1 + (week - 0) * 7; // 1st of January + 7 days for each week
+
+      let dayOffset = new Date(year, 0, 1).getDay(); // we need to know at what day of the week the year start
+
+      dayOffset--; // depending on what day you want the week to start increment or decrement this value. This should make the week start on a monday
+
+      let days = [];
+      for (
+        let i = 0;
+        i < 7;
+        i++ // do this 7 times, once for every day
+      )
+        days.push(new Date(year, 0, day - dayOffset + i));
+      // add a new Date object to the array with an offset of i days relative to the first day of the week
+      return days;
+    };
+    var week = document.querySelector("#date-input");
+    var dates = parseDates(week.value);
+    let days = [];
+    let Z;
+    let w;
+    for (let i = 0; i < 7; i++) {
+      Z = dates[i].toString();
+      w = Z.split(" ");
+      days[i] = w[2];
+    }
+
+    var date = $("#date-input").val().split("-");
+    week = date[1];
+    year = date[0];
+
+    var id = $("#name").val();
+    $("#idCard").html(week);
+
+    alert(id);
+    alert(date);
+    alert(days);
   });
 });
